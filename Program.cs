08dc06383994c9a1d1177c69d5b352e05a75b234
GhostX <egamovkhurshed29@gmail.com>
2020-03-23 09:27:05 +0500

    First home work

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------1-----------
+            // System.Console.WriteLine("--------------1--------------");
+            // double a = 16.80, b = 12.40;
+            // Console.WriteLine(Math.Sqrt(a*b));
+            //-----------2-----------
+            // System.Console.WriteLine("--------------2--------------");
+            // double a = 1.4,b = -5.5,c = 0.60, ac = a - c,bc = c - b;
+            // System.Console.WriteLine(ac);
+            // System.Console.WriteLine(bc);
+            //-----------2-----------
+            // System.Console.WriteLine("--------------3--------------");
+            // double x1 = -6.20, x2 = 2.1, y1 = 5.2, y2 = 9.8;
+            // Console.WriteLine(Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)));
+            //-----------4-----------
+            // System.Console.WriteLine("--------------4--------------");
+            // int a = 41;
+            // System.Console.WriteLine(a%10+""+a/10);
+            //-----------5-----------
+            // System.Console.WriteLine("--------------5--------------");
+            // int n = 10985;
+            // System.Console.WriteLine(n/60);
+            //-----------6-----------
+            // System.Console.WriteLine("--------------6--------------");
+            // int k = 202;
+            // System.Console.WriteLine(k%7);
+            // Console.ReadKey();
+        }
+    }
+}
